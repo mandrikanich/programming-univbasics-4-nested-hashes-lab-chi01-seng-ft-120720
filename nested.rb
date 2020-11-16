d25db230127f811bf0b8df23b871671af8b99a8c
Michael Andrikanich <mandrikanich@gmail.com>
2020-11-16 18:34:30 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper] 
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +55,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +81,11 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto]={
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
+programmer_hash
 end
 
 def changing_alan
@@ -103,7 +107,11 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:alan_kay]={
+        :known_for => "GUI",
+        :languages => ["Smalltalk", "LISP"]
+      }
+programmer_hash
 end
 
 def adding_to_dennis
@@ -122,8 +130,8 @@ def adding_to_dennis
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => ["C", "Assembly"]
       }
     }
-
+programmer_hash
 end
