cd4c8d0b1a9ea080b46df0c9af92e6007cbf8c00
Michael Andrikanich <mandrikanich@gmail.com>
2020-11-16 16:53:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,8 +60,29 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# programmer_hash[:alan_kay][:known_for]z
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#   :languages => ["LISP", "C"]
+# }
 # return the entire updated hash
+
+	programmer_hash = 
+ 		{
+      :grace_hopper => {
+        :known_for => "COBOL",
+        :languages => ["COBOL", "FORTRAN"]
+      },
+      :alan_kay => {
+        :known_for => "Object Orientation",
+        :languages => ["Smalltalk", "LISP"]
+      },
+      :dennis_ritchie => {
+        :known_for => "Unix",
+        :languages => ["C"]
+      }
+    }
+
+end
 
 	programmer_hash = 
  		{
