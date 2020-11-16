e9ca4a97c586b6d032d7d249e6ea45fd049ee72c
Michael Andrikanich <mandrikanich@gmail.com>
2020-11-16 17:55:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -88,23 +88,6 @@ def adding_matz
 programmer_hash[:yukihiro_matsumoto]
 end
 
-	programmer_hash = 
- 		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
-      :dennis_ritchie => {
-        :known_for => "Unix",
-        :languages => ["C"]
-      },
-    }
-end
-
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
